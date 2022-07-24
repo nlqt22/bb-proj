a02e3d9c6510756fb7deb6122c8104e52edccab4
Your Name <you@example.com>
2022-07-24 18:39:06 +0700

    Update Livechart

--- CovidTrackerApplication/MainWindow.Designer.cs
+++ CovidTrackerApplication/MainWindow.Designer.cs
@@ -31,6 +31,9 @@
             this.updateButton = new System.Windows.Forms.Button();
             this.totalInternalCasesBT = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.casesChart = new LiveCharts.Wpf.CartesianChart();
+            this.showCasesBT = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,14 +61,35 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(16, 111);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(387, 315);
+            this.dataGridView1.Size = new System.Drawing.Size(311, 315);
             this.dataGridView1.TabIndex = 2;
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(364, 111);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(415, 315);
+            this.elementHost1.TabIndex = 3;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.casesChart;
+            // 
+            // showCasesBT
+            // 
+            this.showCasesBT.Location = new System.Drawing.Point(364, 19);
+            this.showCasesBT.Name = "showCasesBT";
+            this.showCasesBT.Size = new System.Drawing.Size(75, 23);
+            this.showCasesBT.TabIndex = 4;
+            this.showCasesBT.Text = "Số ca nhiễm";
+            this.showCasesBT.UseVisualStyleBackColor = true;
+            this.showCasesBT.Click += new System.EventHandler(this.showCasesBT_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showCasesBT);
+            this.Controls.Add(this.elementHost1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.totalInternalCasesBT);
             this.Controls.Add(this.updateButton);
@@ -83,5 +107,8 @@
         private System.Windows.Forms.Button updateButton;
         private System.Windows.Forms.Label totalInternalCasesBT;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private LiveCharts.Wpf.CartesianChart casesChart;
+        private System.Windows.Forms.Button showCasesBT;
     }
 }
--- CovidTrackerApplication/MainWindow.cs
+++ CovidTrackerApplication/MainWindow.cs
@@ -9,6 +9,12 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
 
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.WinForms;
+
+using Brushes = System.Windows.Media.Brushes;
+
 using CovidTrackerApplication.utils;
 using CovidTrackerApplication.entity;
 
@@ -21,6 +27,80 @@ namespace CovidTrackerApplication
         {
             InitializeComponent();
         }
+
+        private void Display_CasesChart(Button button)
+        {
+            casesChart.Series.Clear();
+            DailyReport daily = DataTracker.readDailyReport();
+
+            string[] days = new string[7];
+
+            var caseValues = new ChartValues<int>();
+            var deathValues = new ChartValues<int>();
+            var recoverValues = new ChartValues<int>();
+
+            var avgCase7DayValues = new ChartValues<int>();
+            var avgDeath7DayValues = new ChartValues<int>();
+            var avgRecover7DayValues = new ChartValues<int>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                days[i] = daily.Overview[i].Date.ToString();
+                caseValues.Add(daily.Overview[i].Cases);
+                avgCase7DayValues.Add(daily.Overview[i].AvgCases7day);
+                deathValues.Add(daily.Overview[i].Death);
+                avgDeath7DayValues.Add(daily.Overview[i].AvgDeath7day);
+                recoverValues.Add(daily.Overview[i].Recovered);
+                avgRecover7DayValues.Add(daily.Overview[i].AvgRecovered7day);
+            }
+
+            var columnValues = new ChartValues<int>();
+            var lineValues = new ChartValues<int>();
+
+            if (button.Text.Equals("Số ca nhiễm"))
+            {
+                columnValues = caseValues;
+                lineValues = avgCase7DayValues;
+            }
+            else if (button.Text.Equals("Số ca tử vong"))
+            {
+                lineValues = avgDeath7DayValues;
+                columnValues = deathValues;
+            }
+            else if (button.Text.Equals("Số ca khỏi"))
+            {
+                lineValues = avgRecover7DayValues;
+                columnValues = recoverValues;
+            }
+
+
+            ColumnSeries casesColumnSeries = new ColumnSeries
+            {
+                Values = columnValues,
+                DataLabels = true,
+                Title = button.Text
+            };
+            LineSeries avgLineSeries = new LineSeries
+            {
+                Values = avgCase7DayValues,
+                StrokeThickness = 4,
+                Fill = Brushes.Transparent,
+                Title = "Trong 7 ngày"
+            };
+            casesChart.Series.Add(casesColumnSeries);
+            casesChart.Series.Add(avgLineSeries);
+            casesChart.AxisX.Clear();
+            casesChart.AxisX.Add(new Axis
+            {
+                Labels = days,
+                Separator = new Separator
+                {
+                    Step = 1,
+                    IsEnabled = true
+                }
+            });
+        }
+
 
         private void Display_DataTable()
         {
@@ -45,6 +125,7 @@ namespace CovidTrackerApplication
             {
                 updateButton.Text = DataTracker.getSaveFileInfo();
                 Display_DataTable();
+                Display_CasesChart(showCasesBT);
             }
 
         }
@@ -61,6 +142,11 @@ namespace CovidTrackerApplication
                 MessageBox.Show(ex.Message);
             }
 
+        }
+
+        private void showCasesBT_Click(object sender, EventArgs e)
+        {
+            Display_CasesChart(showCasesBT);
         }
     }
 }
